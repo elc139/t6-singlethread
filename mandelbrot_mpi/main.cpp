56fcb7a0018a3e8d0cffc541c539a17a82063b16
Lorenzo <lorenzofman@gmail.com>
2021-07-31 20:30:28 -0300

    Mpi Hello World (at last)

--- mandelbrot_mpi/main.cpp
+++ mandelbrot_mpi/main.cpp
@@ -13,6 +13,24 @@ int main(int argc, char** argv)
     int myRank;
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    if (myRank == 0)
+    {
+        printf("Master process running in %s, rank %d out of %d processors\n", processor_name, myRank, size);
+    }
+    else
+    {
+        printf("Slave process running in %s, rank %d out of %d processors\n", processor_name, myRank, size);
+    }
+
     if (argc == 3)
     {
 //        MandelbrotGenerator::Generate(Parse(argv[2]), Parse(argv[1]));
@@ -20,7 +38,7 @@ int main(int argc, char** argv)
     else
     {
         std::cout << "DEBUG Mode"  << std::endl;
-//        MandelbrotGenerator::Generate(20, 1024);
+        //MandelbrotGenerator::Generate(20, 1024);
     }
 
     MPI_Finalize();
